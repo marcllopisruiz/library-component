96c6c383bb8b01a56db06b8140cdef0c47a9c335
marc llopis <marclr@stradivarius.com>
2023-03-24 09:08:07 +0100

    custom library code

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+type CardProps = {
+  title: string;
+  description: string;
+};
+
+const CardWrapper = styled.div`
+  padding: 10px 20px;
+  border: 1px solid black;
+  margin: 5%;
+  border-radius: 5px;
+  h1 {
+    font-weight: bold;
+    font-size: 2rem;
+  }
+`;
+
+export const Card = ({ title, description }: CardProps) => {
+  return (
+    <CardWrapper>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </CardWrapper>
+  );
+};
